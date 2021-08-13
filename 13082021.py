23a606d311d0f2c6452327dd2e393ad31348ea00
afilashka <afilashka@mail.ru>
2021-08-13 18:55:06 +0500

    #2 значения переменных

--- 13082021.py
+++ 13082021.py
@@ -1,2 +1,6 @@
 import requests
 import pandas
+
+fields = ['first_name', 'last_name', 'sex', 'city', 'bdate', 'education']
+token = '76eb2ca708b9731cc1ddf0904619d1469f261d9aa5b7f90814fb0d4fc04ba53752faceb8b5b861591326d'
+users = [141536612, 139046077, 137464025, 109024405, 203137367, 191107635]
